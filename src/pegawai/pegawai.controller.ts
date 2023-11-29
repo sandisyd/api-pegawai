4c866f6cc3f42d85d6ee687bebd7b9ab551c7cb4
Sandi <33063833+sandisyd@users.noreply.github.com>
2023-11-29 21:09:51 +0700

    Menambahkan create, update, read, delete

--- src/pegawai/pegawai.controller.ts
+++ src/pegawai/pegawai.controller.ts
@@ -1,18 +1,46 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { PegawaiService } from './pegawai.service';
 import { PegawaiDto } from './dto/pegawai.dto';
+import { JsonContains } from 'typeorm';
 
 @Controller('pegawai')
 export class PegawaiController {
   constructor(private pegawaiService: PegawaiService) {}
+
+  //   get all data pegawai
   @Get()
   async getPegawai() {
     return await this.pegawaiService.getPegawai();
   }
 
+  //   Insert data pegawai
   @Post('add_data')
-  async createPegawai(@Body() cPegawai: PegawaiDto
+  async createPegawai(@Body() cPegawai: PegawaiDto) {
+    return this.pegawaiService.createPegawai(cPegawai);
+  }
+
+  //   get pegawai by id
+  @Get(':id')
+  async getPegawaiById(@Param('id') id: string) {
+    return this.pegawaiService.getPegawaiById(id);
+  }
+
+  //   update data pegawai
+  @Post('updatePegawai/:id')
+  async updateDataPegawai(
+    @Param('id') id: string,
+    @Body() uPegawai: PegawaiDto,
   ) {
-    return this.pegawaiService.createPegawai(cPegawai)
+    await this.pegawaiService.updatePegawai(id, uPegawai);
+    return "Data Berhasil di update"
+  }
+
+  // delete data
+  @Delete('deletePegawai/:id')
+  async deleteData(@Param('id') id: string){
+    await this.pegawaiService.deletePegawai(id);
+    return JsonContains({
+        message: "Data berhasil dihapus"
+    });
   }
 }
--- src/pegawai/pegawai.service.ts
+++ src/pegawai/pegawai.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Pegawai } from './pegawai.entity';
-import { Repository } from 'typeorm';
+import { JsonContains, Repository } from 'typeorm';
 import { PegawaiDto } from './dto/pegawai.dto';
 
 @Injectable()
@@ -12,7 +12,7 @@ export class PegawaiService {
 
   // get data
   async getPegawai() {
-    return  this.pegawaiRepo.find();
+    return this.pegawaiRepo.find();
   }
 
   // get data by id
@@ -21,10 +21,21 @@ export class PegawaiService {
   }
 
   // create data pegawai
-  async createPegawai(dPegawai: PegawaiDto
-  ) {
+  async createPegawai(dPegawai: PegawaiDto) {
     const pegawaiNew = this.pegawaiRepo.create(dPegawai);
-    await this.pegawaiRepo.save(pegawaiNew)
+    await this.pegawaiRepo.save(pegawaiNew);
     return pegawaiNew;
+  }
+
+  // update data pegawai
+  async updatePegawai(id: string, udPegawai: PegawaiDto) {
+    await this.pegawaiRepo.update({ id: parseInt(id) }, udPegawai);
+    return await this.pegawaiRepo.findOne({ where: { id: parseInt(id) } });
+  }
+
+  // delete data pegawai
+  async deletePegawai(id: string){
+    
+    return await this.pegawaiRepo.delete(parseInt(id));
   }
 }
